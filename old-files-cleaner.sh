9490769b8e3393ea21dd3e00c05d8f6cb6d70cc6
Erik Bejstam <erikbejstam@hotmail.com>
2025-06-04 23:03:59 +0200

    Add todo comments to cleanscript

--- old-files-cleaner.sh
+++ old-files-cleaner.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Just an initial test script. 
+# Later I probably want to prompt the user with every file to be deleted actually, and the user can go y/n.
+# Should also include possibility to delete directories.
+
 read -pr "Enter directory to clean: " TARGET_DIR 
 
 read -pr "Delete files not modified in how many days: " DAYS

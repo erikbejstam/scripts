99517936780ef3b129a09f7676f6d79c4bde1e2d
erikbejstam <erikbejstam@hotmail.com>
2025-06-17 00:41:57 +0200

    Fix exit statement in ouch-my-eyes

--- ouch-my-eyes.sh
+++ ouch-my-eyes.sh
@@ -5,13 +5,13 @@
 # TODO: Add the possibility to add a second parameter, that is the screen id. Not eeded right now since this is just private use on a specific machine.
 
 if ! [[ "$1" =~ ^[0-9]+$ ]]; then
-	echo "Error: Please provide a number between 10 and 100. Not $1."
+	echo "Error: Please provide a number between 1  and 100. Not $1."
 	exit 1
 fi
 
-if (( $1 < 10 || $1 > 100 )); then
-	echo "Error: The provided argument x must be 10 <= x <= 100."
-	return 1
+if (( $1 < 1 || $1 > 100 )); then
+	echo "Error: The provided argument x must be 1 <= x <= 100."
+	exit 1
 fi
 
 echo "Setting brightness to $1"
